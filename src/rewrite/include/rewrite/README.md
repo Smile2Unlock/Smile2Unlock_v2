d05fc5272391971f2dc4f528d7ee3bac643a7c4c
ation_ciger <ation_ciger@sgxi.cn>
2026-04-20 22:00:55 +0800

    remove all rewrite

--- src/rewrite/include/rewrite/README.md
+++
@@ -1,1 +0,0 @@
-This directory stores public rewrite-era headers when a module needs a non-module bridge.
--- src/rewrite/windows/credential_provider_placeholder.cpp
+++
@@ -1,5 +0,0 @@
-#include <windows.h>
-
-extern "C" __declspec(dllexport) HRESULT RewriteCredentialProviderPlaceholder() {
-    return S_OK;
-}
